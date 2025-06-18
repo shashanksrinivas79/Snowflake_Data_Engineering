be3b83081cdeca4fe058b8a6f443bcdae4e6e097
Shashank Srinivas Midididdi <shashankmidididdi@Shashanks-MacBook-Air.local>
2025-06-18 15:25:21 -0700

    changed chapter - 2 and chapter - 3 sql files correctly

--- Chapter - 3/Chapter_03.sql
+++ Chapter - 3/Chapter_03.sql
@@ -1,18 +1,66 @@
+-- create a storage integration
+-- using Microsoft Azure
+-- refer to Chapter 4 for Amazon S3
+use role ACCOUNTADMIN;
+
+create storage integration BISTRO_INTEGRATION
+  type = external_stage
+  storage_provider = 'AZURE'
+  enabled = true
+  azure_tenant_id = '1234abcd-xxx-56efgh78' --use your own Tenant ID
+  storage_allowed_locations = ('azure://bakeryorders001.blob.core.windows.net/orderfiles/');
+
+-- describe the storage integration and take note of the following parameters:
+-- - AZURE_CONSENT_URL
+-- - AZURE_MULTI_TENANT_APP_NAME
+describe storage integration BISTRO_INTEGRATION;
+
+-- grant usage on storage integration so that the SYSADMIN role can use it
+grant usage on integration BISTRO_INTEGRATION to role SYSADMIN;
+
+-- create a new schema in the BAKERY_DB database (see Chapter 2)
 use role SYSADMIN;
-create database BAKERY_DB;
-create schema ORDERS;
-
+create warehouse if not exists BAKERY_WH with warehouse_size = 'XSMALL';
+create database if not exists BAKERY_DB;
 use database BAKERY_DB;
-use schema ORDERS;
-create stage ORDERS_STAGE;
+create schema EXTERNAL_ORDERS;
+use schema EXTERNAL_ORDERS;
 
-list @ORDERS_STAGE;
+-- create an external stage using the storage integration
+-- Listing 3.1 
+create stage BISTRO_STAGE
+  storage_integration = BISTRO_INTEGRATION
+  url = 'azure://bakeryorders001.blob.core.windows.net/orderfiles';
 
-select $1, $2, $3, $4, $5 from @ORDERS_STAGE;
+-- Upload a sample file named Orders_2023-08-04.csv to the storage container
 
+-- view files in the external stage
+list @BISTRO_STAGE;
+
+-- create an external stage using a SAS token
+create stage BISTRO_SAS_STAGE
+  URL = 'azure://bakeryorders001.blob.core.windows.net/orderfiles'
+  CREDENTIALS=(AZURE_SAS_TOKEN = '?sv=2023-...%3D'); --generate and use your own SAS token
+
+-- view files in the external stage
+list @BISTRO_SAS_STAGE;
+
+-- create a named file format
+create file format ORDERS_CSV_FORMAT
+  type = csv
+  field_delimiter = ','
+  skip_header = 1;
+
+-- create the external stage by adding the file format
+create or replace stage BISTRO_STAGE
+  storage_integration = BISTRO_INTEGRATION
+  url = 'azure://bakeryorders001.blob.core.windows.net/orderfiles'
+  file_format = ORDERS_CSV_FORMAT;
+
+-- create staging table for restaurant orders
 use database BAKERY_DB;
-use schema ORDERS;
-create table ORDERS_STG(
+use schema EXTERNAL_ORDERS;
+create table ORDERS_BISTRO_STG (
   customer varchar,
   order_date date,
   delivery_date date,
@@ -22,120 +70,80 @@ create table ORDERS_STG(
   load_ts timestamp
 );
 
-use database BAKERY_DB;
-use schema ORDERS;
-copy into ORDERS_STG
-from(
- select $1, $2, $3, $4, $5, metadata$filename, current_timestamp(), from @ORDERS_STAGE
+-- load data from the stage into the staging table
+-- Listing 3.3 
+copy into ORDERS_BISTRO_STG
+from (
+  select $1, $2, $3, $4, $5, metadata$filename, current_timestamp() 
+  from @BISTRO_STAGE
 )
-file_format = (type = CSV, skip_header = 1)
+file_format = ORDERS_CSV_FORMAT
 on_error = abort_statement
-purge = true;
+;
 
-select * from ORDERS_STG;
+-- view data in the staging table
+select * from ORDERS_BISTRO_STG;
 
-list @ORDERS_STAGE;
+-- view load history for the table
+select *
+from information_schema.load_history
+where schema_name = 'EXTERNAL_ORDERS' and table_name = 'ORDERS_BISTRO_STG'
+order by last_load_time desc;
 
+-- add a directory table to the stage
+alter stage BISTRO_STAGE
+set directory = (enable = true);
+
+-- manually refresh the directory
+alter stage BISTRO_STAGE refresh;
+
+-- query the directory table
+select * 
+from directory (@BISTRO_STAGE);
+
+-- Upload additional CSV files to the container in the 202308 path
+
+-- load data from the stage into the staging table by specifying a path
+copy into ORDERS_BISTRO_STG
+from (
+  select $1, $2, $3, $4, $5, metadata$filename, current_timestamp() 
+  from @BISTRO_STAGE/202308
+)
+file_format = ORDERS_CSV_FORMAT
+on_error = abort_statement
+;
+
+-- create an external table 
+-- Listing 3.4 
 use database BAKERY_DB;
-use schema ORDERS;
-create table CUSTOMER_ORDERS(
-  customer varchar,
-  order_date date,
-  delivery_date date,
-  baked_good_type varchar,
-  quantity number,
-  source_file_name varchar,
-  load_ts timestamp
-);
+use schema EXTERNAL_ORDERS;
+create external table ORDERS_BISTRO_EXT (
+  customer varchar as (VALUE:c1::varchar),
+  order_date date as (VALUE:c2::date),
+  delivery_date date as (VALUE:c3::date),
+  baked_good_type varchar as (VALUE:c4::varchar),
+  quantity number as (VALUE:c5::number),
+  source_file_name varchar as metadata$filename
+)
+location = @BISTRO_STAGE
+auto_refresh = FALSE
+file_format = ORDERS_CSV_FORMAT;
 
--- target table
-merge into CUSTOMER_ORDERS tgt
--- source table
-using ORDERS_STG as src
--- primary keys/ uniqueness
-on src.customer = tgt.customer
-    and src.delivery_date = tgt.delivery_date
-    and src.baked_good_type = tgt.baked_good_type
--- when matched update
-when matched then
-    update set tgt.quantity = src.quantity,
-        tgt.source_file_name = src.source_file_name,
-        tgt.load_ts = current_timestamp()
--- when not matched insert new values from src to tgt
-when not matched then
-    insert (customer, order_date, delivery_date, baked_good_type, quantity, source_file_name, load_ts)
-    values (src.customer, src.order_date, src.delivery_date, src.baked_good_type, src.quantity, src.source_file_name, current_timestamp());
+-- query the external table
+select * 
+from ORDERS_BISTRO_EXT;
 
-select * from customer_orders order by delivery_date, quantity desc;
+-- refresh the external table
+alter external table ORDERS_BISTRO_EXT refresh;
 
+-- create a materialized view
 use database BAKERY_DB;
-use schema ORDERS;
-create table SUMMARY_ORDERS(
- delivery_date date,
- baked_good_type varchar,
- total_quantity number
-);
+use schema EXTERNAL_ORDERS;
+create materialized view ORDERS_BISTRO_MV as
+select customer, order_date, delivery_date, 
+  baked_good_type, quantity, source_file_name
+from ORDERS_BISTRO_EXT;
 
-select delivery_date, baked_good_type, sum(quantity) as total_quantity from CUSTOMER_ORDERS
-group by all;
-
-truncate table SUMMARY_ORDERS;
-
-insert into SUMMARY_ORDERS(delivery_date, baked_good_type, total_quantity)
- select delivery_date, baked_good_type, sum(quantity) as total_quantity from CUSTOMER_ORDERS
- group by all;
-
-select * from SUMMARY_ORDERS;
-
-use database BAKERY_DB;
-use schema ORDERS;
-create task PROCESS_ORDERS
- warehouse = BAKERY_WH
- schedule = '10 M'
-as
-begin
- truncate table ORDERS_STG;
- copy into ORDERS_STG
- from (
-  select $1, $2, $3, $4, $5, metadata$filename, current_timestamp()
-  from @ORDERS_STAGE
- )
- file_format = (type = CSV, skip_header = 1)
- on_error = abort_statement
- purge = true;
-
- merge into CUSTOMER_ORDERS tgt
- using ORDERS_STG as src
- on src.customer = tgt.customer
-  and src.delivery_date = tgt.delivery_date
-  and src.baked_good_type = tgt.baked_good_type
- when matched then
-  update set tgt.quantity = src.quantity,
-      tgt.source_file_name = src.source_file_name,
-      tgt.load_ts = current_timestamp()
- when not matched then
-  insert (customer, order_date, delivery_date, baked_good_type, quantity, source_file_name, load_ts)
-    values (src.customer, src.order_date, src.delivery_date, src.baked_good_type, src.quantity, src.source_file_name,     current_timestamp());
-    
-  truncate table SUMMARY_ORDERS;
-  insert into SUMMARY_ORDERS(delivery_date, baked_good_type, total_quantity)
-    select delivery_date, baked_good_type, sum(quantity) as total_quantity
-    from CUSTOMER_ORDERS
-    group by all;
-end;
-
-use role accountadmin;
-grant execute task on account to role sysadmin;
-use role sysadmin;
-
-execute task PROCESS_ORDERS;
-
-select * from table(information_schema.task_history())
- order by scheduled_time desc;
-
-alter task PROCESS_ORDERS resume;
-
-alter task PROCESS_ORDERS suspend;
-alter task PROCESS_ORDERS
-set schedule = 'USING CRON 0 23 * * * UTC';
-alter task PROCESS_ORDERS resume;
+-- query the materialized view
+select * 
+from ORDERS_BISTRO_MV;
